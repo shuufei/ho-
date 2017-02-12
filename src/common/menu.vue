32b93ed01e3b002a7931dce22a019eecffb96dee
shuufei <digital.shuufei@gmail.com>
2017-02-12 21:33:05 +0900

    Add hambuger menu action.

--- src/common/menu.vue
+++ src/common/menu.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 div#menu
-  div.g-menu-icon
-    button.hamburger.hamburger--squeeze.g-menu(type="button" v-on:click="changeHamburgerState" v-bind:class="getHamburgerState()")
-      span.hamburger-box.g-menu
-        span.hamburger-inner.g-menu
+  div.global-menu(v-on:click="openMenu()")
+    span.top(v-bind:class="{ 'active': menuOpen, 'inactive': menuClose }")
+    span.middle(v-bind:class="{ 'active': menuOpen, 'inactive': menuClose }")
+    span.bottom(v-bind:class="{ 'active': menuOpen, 'inactive': menuClose }")
 </template>
 
 <script>
@@ -16,10 +16,20 @@ export default {
       travelInfo: '旅INFO',
       about: 'ABOUT',
       gMenuOpen: false,
-      hamburgerActive: false
+      hamburgerActive: false,
+      menuOpen: false,
+      menuClose: false
     }
   },
   methods: {
+    openMenu: function (event) {
+      if (this.menuOpen) {
+        this.menuClose = true;
+      } else {
+        this.menuClose = false;
+      }
+      this.menuOpen = !this.menuOpen;
+    },
     chageMenuState: function (event) {
       this.gMenuOpen = !this.gMenuOpen;
     },
@@ -48,28 +58,108 @@ export default {
 :root {
   --main-color: #99CDFF;
 }
+.global-menu, .global-menu span {
+  transition: all .4s;
+  box-sizing: border-box;
+}
 
-.g-menu-icon {
-  position: fixed;;
-  top: 46.5px;
-  left: 80px;
+.global-menu {
+  position: fixed;
+  top: 60px;
+  left: 95px;
+  background: white;
+  height: calc(3px * 3 + 11px * 2);
+  width: 40px;
   z-index: 100;
 }
 
-.hamburger-inner {
-  color: var(--main-color);
-  background: var(--main-color);
+.global-menu span {
+    position: absolute;
+    height: 4px;
+    width: 40px;
+    left: 0;
+    background: var(--main-color);
+    border-radius: 3px;
 }
 
-.hamburger-inner::before {
-  background: var(--main-color);
+.top {
+  top: 0;
+}
+.middle {
+  top: 11px;
+}
+.bottom {
+  top: 22px;
 }
 
-.hamburger-inner::after {
-  background: var(--main-color);
+.top.active {
+  animation: menu-top-open 1s forwards;
+}
+.top.inactive {
+  animation: menu-top-close 1s forwards;
 }
 
+@keyframes menu-top-open {
+  0% {
+		transform: translateY(0) rotate(0);
+	}
+	50% {
+		transform: translateY(11px) rotate(0);
+	}
+	100% {
+		transform: translateY(11px) rotate(45deg);
+	}
+}
+@keyframes menu-top-close {
+  0% {
+    transform: translateY(11px) rotate(45deg);
+  }
+  50% {
+    transform: translateY(11px) rotate(0);
+  }
+  100% {
+    transform: translateY(0) rotate(0);
+  }
+}
 
+.middle.active {
+  transition: all 0.25s 0.5s;
+  opacity: 0;
+}
+.middle.inactive {
+  transition: all 0.25s 0.5s;
+  opacity: 1;
+}
+
+.bottom.active {
+  animation: menu-bottom-open 1s forwards;
+}
+.bottom.inactive {
+  animation: menu-bottom-close 1s forwards;
+}
+
+@keyframes menu-bottom-open {
+  0% {
+		transform: translateY(0) rotate(0);
+	}
+	50% {
+		transform: translateY(-11px) rotate(0);
+	}
+	100% {
+		transform: translateY(-11px) rotate(-45deg);
+	}
+}
+@keyframes menu-bottom-close {
+  0% {
+    transform: translateY(-11px) rotate(-45deg);
+  }
+  50% {
+    transform: translateY(-11px) rotate(0);
+  }
+  100% {
+    transform: translateY(0) rotate(0);
+  }
+}
 
 /*
 

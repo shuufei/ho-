bad5db1870430d1081a6cb8c0b14ef137a13eda3
shuufei <digital.shuufei@gmail.com>
2017-03-05 22:19:33 +0900

    fixed about page.

--- src/about/about.vue
+++ src/about/about.vue
@@ -4,19 +4,88 @@ div#about
   div.title-block
     div.title-under-line
       p.title {{ pageTitle }}
+  p.hello-okinawa Hello Okinawa!
+  div.about-description
+    p.about-line 「もっと身近におきなわを」をコンセプトに沖縄に関する情報を情報発信するサイト。
+    p.about-line BLOGと旅INFOというコンテンツを通して、沖縄の暮らしと土地を感じることができるよう制作しています。
+    p.about-line みなさんにとって沖縄をより身近に、そして、より好きになってもらえれば幸いです。
+  div.about-members
+    p.members-title MEMBER
+    div.members
+      member.member-component(v-for="member in members"
+        v-bind:id="member.id"
+        v-bind:name="member.name"
+        v-bind:image="member.image"
+        v-bind:work="member.work"
+        v-bind:description="member.description"
+        v-bind:sns="member.sns")
+  div.fotter
+    p.fotter-ho-title HO!
+    p.fotter-ho-sub-title もっと身近におきなわを
+
+
 </template>
 
 <script>
 import menu from '../common/menu.vue'
+import member from './member.vue'
 
 export default {
   name: 'about',
   components: {
-    'global-menu': menu
+    'global-menu': menu,
+    'member': member
   },
   data () {
     return {
-      pageTitle: 'ABOUT'
+      pageTitle: 'ABOUT',
+      members: [
+        {
+          id: 1,
+          name: 'ふみふみ',
+          image: '/dist/atufumi.jpg',
+          work: 'bloger',
+          description: '沖縄を愛し、沖繩に舞い戻ってきた男。普段は名護を中心に行動している。  趣味は美味しいお店巡るのと、写真。  嫌いなものは、KP女子だ。',
+          sns: {
+            twitter: 'true',
+            instagram: 'true',
+            facebook: 'true'
+          }
+        },
+        {
+          id: 2,
+          name: 'ハマヒロ',
+          image: '/dist/masahiro.jpg',
+          work: 'bloger',
+          description: 'ハマケンを目指す22歳。主に沖縄の名護で活動している。好きなのはおしゃれなもの。趣味はカメラとハマケン。',
+          sns: {
+            twitter: 'true',
+            instagram: 'true'
+          }
+        },
+        {
+          id: 3,
+          name: 'パウエル',
+          image: '/dist/ryusuke.jpg',
+          work: 'bloger',
+          description: '沖縄で働いている人間。沖縄の勝ち組企業で働いている。顔もいいのでモテるが、残念ながら長い付き合いの彼女がいる。彼に弱点はない。',
+          sns: {
+            twitter: 'true',
+            instagram: 'true',
+            facebook: 'true'
+          }
+        },
+        {
+          id: 4,
+          name: 'fei',
+          image: '/dist/fei.jpg',
+          work: 'engineer',
+          description: 'エンジニアをしている人間。唯一東京在住の人間。デザインとエンジニアの力を合わせていいものを作りたいと日々何かしらやっている。このサイトの制作を担当。',
+          sns: {
+            twitter: 'true'
+          }
+        }
+      ]
     }
   },
   props: [],
@@ -32,6 +101,7 @@ export default {
 :root {
   --main-color: #99CDFF;
   --title-color: #575757;
+  --fotter-color: #95989A;
   --main-font: avenir;
   --sub-font: "Noto Sans Japanese";
   --hover-hand: {
@@ -72,11 +142,80 @@ export default {
   }
 }
 
+.hello-okinawa {
+  color: var(--main-color);
+  font-family: susan;
+  font-size: 105px;
+  letter-spacing: 10px;
+  margin: 0;
+  margin-top: 150px;
+  text-align: center;
+}
+
+.about-description {
+  margin: 0;
+  margin-top: 40px;
+  & .about-line {
+    color: var(--title-color);
+    font-family: var(--sub-font);
+    font-size: 16px;
+    font-weight: 300;
+    letter-spacing: 2px;
+    margin: 0;
+    margin-top: 16px;
+  }
+}
+
+.about-members {
+  margin: 0;
+  margin-top: 150px;
+  & .members-title {
+    color: var(--title-color);
+    font-family: var(--main-font);
+    font-size: 20px;
+    font-weight: 600;
+    letter-spacing: 2px;
+  }
+  & .members {
+    padding-bottom: 50px;
+    margin: 0;
+    margin-top: 65px;
+    & .member-component {
+      margin-bottom: 100px;
+    }
+  }
+}
 
 .menu-component {
   position: fixed;
   top: 0;
   left: 0;
   z-index: 100;
+}
+
+/*footer*/
+.fotter {
+  display: flex;
+  position: absolute;
+  left: 130px;
+}
+
+.fotter-ho-title {
+  font-family: susan;
+  font-size: 32px;
+  letter-spacing: 4px;
+  color: var(--fotter-color);
+  padding-top: 0;
+  padding-bottom: 25px;
+  margin: 0;
+  margin-right: 5px;
+}
+
+.fotter-ho-sub-title {
+  font-family: var(--sub-font);
+  font-weight: 300;
+  font-size: 10px;
+  letter-spacing: 1px;
+  color: var(--fotter-color);
 }
 </style>
---
+++ src/about/member.vue
@@ -0,0 +1,125 @@
+<template lang="pug">
+div#member
+  img(v-bind:src="image")
+  div.profile
+    p.name {{ name }}
+    p.work {{ work }}
+    p.description {{ description }}
+    div.flex-wrapper
+      div.sns-link
+        i.icon-twitter(v-if="sns.twitter")
+        i.icon-facebook(v-if="sns.facebook")
+        i.icon-instagram(v-if="sns.instagram")
+      p.articles-link 記事一覧へ
+</template>
+
+<script>
+export default {
+  name: 'member',
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  props: [
+    'id',
+    'name',
+    'image',
+    'work',
+    'description',
+    'sns'
+  ],
+  methods: {
+    __method__: function (event) {
+
+    }
+  }
+}
+</script>
+
+<style>
+:root {
+  --main-color: #99CDFF;
+  --title-color: #575757;
+  --main-font: avenir;
+  --sub-font: "Noto Sans Japanese";
+  --hover-hand: {
+    cursor: pointer;
+    cursor: hand;
+  }
+}
+
+#member {
+  color: var(--title-color);
+  display: flex;
+  justify-content: center;
+  & img {
+    size: 100px;
+    object-fit: cover;
+    border-radius: 50%;
+    box-shadow: 0 3px 3px rgba(0, 0, 0, 0.15);
+    margin-right: 40px;
+    margin-top: 20px;
+  }
+  & .profile {
+    text-align: left;
+    & .name {
+      font-family: var(--sub-font);
+      font-size: 18px;
+      font-weight: 300;
+      letter-spacing: 1px;
+      margin: 0;
+    }
+    & .work {
+      font-family: var(--main-font);
+      font-size: 14px;
+      font-style: oblique;
+      font-weight: 200;
+      letter-spacing: 1px;
+      margin: 0;
+      margin-top: 5px;
+    }
+    & .description {
+      font-family: var(--sub-font);
+      font-size: 13px;
+      font-weight: 300;
+      letter-spacing: 1px;
+      margin: 0;
+      margin-top: 20px;
+      width: 320px;
+    }
+    & .flex-wrapper {
+      align-items: center;
+      display: flex;
+      margin: 0;
+      margin-top: 15px;
+      & .sns-link {
+        margin-right: 200px;
+        & i {
+          font-size: 18px;
+          margin-right: 15px;
+          @apply --hover-hand;
+        }
+        & .icon-twitter {
+          color: #1DA1F2;
+        }
+        & .icon-facebook {
+          color: #3B5998;
+        }
+        & .icon-instagram {
+          color: #E4405F;
+        }
+      }
+      & .articles-link {
+        font-family: var(--sub-font);
+        font-size: 14px;
+        font-weight: 300;
+        @apply --hover-hand;
+      }
+    }
+  }
+}
+</style>
